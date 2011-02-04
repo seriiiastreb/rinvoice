2e9b1561f89b65222ac98b65fdb612f63729e8cf
seriiiastreb@gmail.com <seriiiastreb@gmail.com@56977a4c-279a-bab8-0dcc-536525d4a867>
2011-02-04 10:51:44 +0000

    added empty elements on Users form

--- trunk/Sources/rInvoice/rInvoice/UsersInputPanel.Designer.cs
+++ trunk/Sources/rInvoice/rInvoice/UsersInputPanel.Designer.cs
@@ -28,19 +28,265 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.addOrEditUserTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.passwordConfirmLabel = new System.Windows.Forms.Label();
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.passwordConfirmTextBox = new System.Windows.Forms.TextBox();
+            this.rolesComboBox = new System.Windows.Forms.ComboBox();
+            this.roleLabel = new System.Windows.Forms.Label();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.activeUserCheckBox = new System.Windows.Forms.CheckBox();
+            this.userIdTextBox = new System.Windows.Forms.TextBox();
+            this.setPasswordCheckBox = new System.Windows.Forms.CheckBox();
+            this.addOrEditUserTableLayoutPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // addOrEditUserTableLayoutPanel
+            // 
+            this.addOrEditUserTableLayoutPanel.ColumnCount = 4;
+            this.addOrEditUserTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.addOrEditUserTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 82F));
+            this.addOrEditUserTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 81F));
+            this.addOrEditUserTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.loginLabel, 0, 0);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.loginTextBox, 1, 0);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.firstNameLabel, 0, 1);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.lastNameLabel, 0, 2);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.passwordLabel, 0, 5);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.passwordConfirmLabel, 0, 6);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.firstNameTextBox, 1, 1);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.lastNameTextBox, 1, 2);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.passwordTextBox, 1, 5);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.passwordConfirmTextBox, 1, 6);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.rolesComboBox, 1, 3);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.roleLabel, 0, 3);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.cancelButton, 1, 8);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.saveButton, 2, 8);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.activeUserCheckBox, 0, 7);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.userIdTextBox, 2, 7);
+            this.addOrEditUserTableLayoutPanel.Controls.Add(this.setPasswordCheckBox, 0, 4);
+            this.addOrEditUserTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.addOrEditUserTableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.addOrEditUserTableLayoutPanel.Name = "addOrEditUserTableLayoutPanel";
+            this.addOrEditUserTableLayoutPanel.RowCount = 10;
+            this.addOrEditUserTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.addOrEditUserTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.addOrEditUserTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.addOrEditUserTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.addOrEditUserTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.addOrEditUserTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.addOrEditUserTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.addOrEditUserTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.addOrEditUserTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.addOrEditUserTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.addOrEditUserTableLayoutPanel.Size = new System.Drawing.Size(244, 232);
+            this.addOrEditUserTableLayoutPanel.TabIndex = 7;
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Location = new System.Drawing.Point(3, 0);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(33, 13);
+            this.loginLabel.TabIndex = 0;
+            this.loginLabel.Text = "Login";
+            // 
+            // loginTextBox
+            // 
+            this.addOrEditUserTableLayoutPanel.SetColumnSpan(this.loginTextBox, 2);
+            this.loginTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loginTextBox.Location = new System.Drawing.Point(83, 3);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(157, 20);
+            this.loginTextBox.TabIndex = 1;
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(3, 24);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(57, 13);
+            this.firstNameLabel.TabIndex = 2;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(3, 48);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(58, 13);
+            this.lastNameLabel.TabIndex = 3;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(3, 120);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(53, 13);
+            this.passwordLabel.TabIndex = 4;
+            this.passwordLabel.Text = "Password";
+            // 
+            // passwordConfirmLabel
+            // 
+            this.passwordConfirmLabel.AutoSize = true;
+            this.passwordConfirmLabel.Location = new System.Drawing.Point(3, 144);
+            this.passwordConfirmLabel.Name = "passwordConfirmLabel";
+            this.passwordConfirmLabel.Size = new System.Drawing.Size(52, 26);
+            this.passwordConfirmLabel.TabIndex = 5;
+            this.passwordConfirmLabel.Text = "Confirm password";
+            // 
+            // firstNameTextBox
+            // 
+            this.addOrEditUserTableLayoutPanel.SetColumnSpan(this.firstNameTextBox, 2);
+            this.firstNameTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.firstNameTextBox.Location = new System.Drawing.Point(83, 27);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(157, 20);
+            this.firstNameTextBox.TabIndex = 6;
+            // 
+            // lastNameTextBox
+            // 
+            this.addOrEditUserTableLayoutPanel.SetColumnSpan(this.lastNameTextBox, 2);
+            this.lastNameTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lastNameTextBox.Location = new System.Drawing.Point(83, 51);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(157, 20);
+            this.lastNameTextBox.TabIndex = 7;
+            // 
+            // passwordTextBox
+            // 
+            this.addOrEditUserTableLayoutPanel.SetColumnSpan(this.passwordTextBox, 2);
+            this.passwordTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.passwordTextBox.Location = new System.Drawing.Point(83, 123);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.Size = new System.Drawing.Size(157, 20);
+            this.passwordTextBox.TabIndex = 8;
+            // 
+            // passwordConfirmTextBox
+            // 
+            this.addOrEditUserTableLayoutPanel.SetColumnSpan(this.passwordConfirmTextBox, 2);
+            this.passwordConfirmTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.passwordConfirmTextBox.Location = new System.Drawing.Point(83, 147);
+            this.passwordConfirmTextBox.Name = "passwordConfirmTextBox";
+            this.passwordConfirmTextBox.PasswordChar = '*';
+            this.passwordConfirmTextBox.Size = new System.Drawing.Size(157, 20);
+            this.passwordConfirmTextBox.TabIndex = 9;
+            // 
+            // rolesComboBox
+            // 
+            this.addOrEditUserTableLayoutPanel.SetColumnSpan(this.rolesComboBox, 2);
+            this.rolesComboBox.DisplayMember = "Name";
+            this.rolesComboBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rolesComboBox.FormattingEnabled = true;
+            this.rolesComboBox.Location = new System.Drawing.Point(83, 75);
+            this.rolesComboBox.Name = "rolesComboBox";
+            this.rolesComboBox.Size = new System.Drawing.Size(157, 21);
+            this.rolesComboBox.TabIndex = 12;
+            this.rolesComboBox.ValueMember = "Code";
+            // 
+            // roleLabel
+            // 
+            this.roleLabel.AutoSize = true;
+            this.roleLabel.Location = new System.Drawing.Point(3, 72);
+            this.roleLabel.Name = "roleLabel";
+            this.roleLabel.Size = new System.Drawing.Size(29, 13);
+            this.roleLabel.TabIndex = 13;
+            this.roleLabel.Text = "Role";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(83, 203);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(74, 26);
+            this.cancelButton.TabIndex = 11;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(165, 203);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(74, 26);
+            this.saveButton.TabIndex = 10;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // activeUserCheckBox
+            // 
+            this.activeUserCheckBox.AutoSize = true;
+            this.activeUserCheckBox.Checked = true;
+            this.activeUserCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.addOrEditUserTableLayoutPanel.SetColumnSpan(this.activeUserCheckBox, 2);
+            this.activeUserCheckBox.Location = new System.Drawing.Point(3, 179);
+            this.activeUserCheckBox.Name = "activeUserCheckBox";
+            this.activeUserCheckBox.Size = new System.Drawing.Size(79, 17);
+            this.activeUserCheckBox.TabIndex = 14;
+            this.activeUserCheckBox.Text = "Active user";
+            this.activeUserCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // userIdTextBox
+            // 
+            this.userIdTextBox.Location = new System.Drawing.Point(165, 179);
+            this.userIdTextBox.Name = "userIdTextBox";
+            this.userIdTextBox.Size = new System.Drawing.Size(54, 20);
+            this.userIdTextBox.TabIndex = 15;
+            this.userIdTextBox.Visible = false;
+            // 
+            // setPasswordCheckBox
+            // 
+            this.setPasswordCheckBox.AutoSize = true;
+            this.addOrEditUserTableLayoutPanel.SetColumnSpan(this.setPasswordCheckBox, 2);
+            this.setPasswordCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.setPasswordCheckBox.Location = new System.Drawing.Point(3, 99);
+            this.setPasswordCheckBox.Name = "setPasswordCheckBox";
+            this.setPasswordCheckBox.Size = new System.Drawing.Size(156, 18);
+            this.setPasswordCheckBox.TabIndex = 16;
+            this.setPasswordCheckBox.Text = "Set password";
+            this.setPasswordCheckBox.UseVisualStyleBackColor = true;
             // 
             // UsersInputPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 317);
+            this.ClientSize = new System.Drawing.Size(244, 232);
+            this.Controls.Add(this.addOrEditUserTableLayoutPanel);
             this.Name = "UsersInputPanel";
-            this.Text = "Users Input Panel";
+            this.Text = "Enter Data";
+            this.addOrEditUserTableLayoutPanel.ResumeLayout(false);
+            this.addOrEditUserTableLayoutPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel addOrEditUserTableLayoutPanel;
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.Label passwordConfirmLabel;
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.TextBox passwordConfirmTextBox;
+        private System.Windows.Forms.ComboBox rolesComboBox;
+        private System.Windows.Forms.Label roleLabel;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.CheckBox activeUserCheckBox;
+        private System.Windows.Forms.TextBox userIdTextBox;
+        private System.Windows.Forms.CheckBox setPasswordCheckBox;
     }
 }
