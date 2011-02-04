5989816ff045ab16083b2f83ead707f53947dc80
seriiiastreb@gmail.com <seriiiastreb@gmail.com@56977a4c-279a-bab8-0dcc-536525d4a867>
2011-02-04 14:26:46 +0000

    added 2 classes adn added 2 tables in DB

---
+++ trunk/Sources/rInvoice/rInvoice.Utils/DataValidator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace rInvoice.Utils
+{
+    class dataValidator
+    {
+        string mfilename = string.Empty;
+
+        public string fileName
+        {
+            get { return mfilename; }
+            set { mfilename = value; }
+        }
+
+        public dataValidator()
+        {
+        }
+
+        public bool isFieldNumber(string fieldName)
+        {
+            bool result = false;
+            string pattern = @"^[0-9]$";
+            if (fieldName == string.Empty)
+            {
+                result = true;
+            }
+            else if (Regex.IsMatch(fieldName, pattern)) { result = true; }
+
+            return result;
+        }
+
+        public bool isEmail(string inputEmail)
+        {
+            bool result = false;
+
+            System.Text.RegularExpressions.Regex rEMail = new System.Text.RegularExpressions.Regex(@"^[a-zA-Z][\w\.-]{2,28}[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$");
+
+            if (rEMail.IsMatch(inputEmail) && inputEmail.Length > 0)
+            { result = true; }
+
+            return result;
+        }
+
+        public bool IsNumber(string strNumber)
+        {
+            // TO DO My bi use --- TRYPARCE
+
+            bool result = false;
+            string exp1 = @"^[-+]?[0-9]*\.?[0-9]+$";
+            string exp2 = @"^[-+]?[0-9]*\,?[0-9]+$";
+            Regex regex = new Regex("(" + exp1 + ")|(" + exp2 + ")");
+            if (strNumber == string.Empty) { result = true; }
+            else if (regex.IsMatch(strNumber) && strNumber.Length > 0) { result = true; }
+
+            return result;
+        }
+    }
+}
---
+++ trunk/Sources/rInvoice/rInvoice.Utils/UFT.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rInvoice.Utils
+{
+    public class UFT
+    {
+        public class Table_TableList
+        {
+            public static readonly string TABLE_NAME = "TablesList";
+            public static readonly string COLUMN_TableID = "TableID";
+            public static readonly string COLUMN_TableNameDB = "TableNameDB";
+            public static readonly string COLUMN_TableNameForUser = "TableNameForUser";
+        }
+
+        public class Table_FieldsList
+        {
+            public static readonly string TABLE_NAME = "FieldsList";
+            public static readonly string COLUMN_TableID = "TableID";
+            public static readonly string COLUMN_FieldID = "FieldID";
+            public static readonly string COLUMN_FieldNameDB = "FieldNameDB";
+            public static readonly string COLUMN_FieldNameForUser = "FieldNameForUser";
+            public static readonly string COLUMN_ClassifierType = "ClassifierType";
+        }
+
+    }
+}
